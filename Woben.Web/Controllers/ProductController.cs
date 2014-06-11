159c1977503f8279c5e0266431a79e2693eb9cd9
Yago Pérez Vázquez <yagoperez@outlook.com>
2014-06-11 15:46:16 +0200

    Bug PC

--- Woben.Web/Controllers/ProductController.cs
+++ Woben.Web/Controllers/ProductController.cs
@@ -113,8 +113,7 @@ namespace Woben.Web.Controllers
                         var originalImage = await db.Images.Where(i => i.Identity == image.Identity).Include(i => i.Products).FirstAsync();
                         if (originalImage != null)
                         {
-                            product.Images.Remove(image);
-                            originalImage.Products.Remove(product);                            
+                            product.Images.Remove(image);                            
                         }
                     }
                 }
